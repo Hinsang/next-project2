edb89b30c915cb98119aaed59b0f58a12eee2ee4
황인상 <star4381@naver.com>
2022-05-23 03:51:02 +0900

    next-project

--- client/pages/Studies.js
+++ client/pages/Studies.js
@@ -3,32 +3,32 @@ import React from 'react';
 // import styles from '../components/Studies1.module.css';
 import StudiesSection from '../components/StudiesSection';
 
-const Studies = () =>
-  // const [count, setCount] = useState(0);
-  // const [text, setText] = useState('');
-  // const [textDone2, setTextDone2] = useState(false);
-  // const sentence = '공부한 내용들';
-  // useEffect(() => {
-  //   const typing = setInterval(() => {
-  //     setText(text + sentence[count]);
-  //     setCount(count + 1);
-  //   }, 150);
-  //   if (sentence.length === count) {
-  //     clearInterval(typing);
-  //     setTimeout(() => {
-  //       setTextDone2(true);
-  //     }, 1000);
-  //   }
-  //   return () => {
-  //     clearInterval(typing);
-  //   };
-  // });
-  // eslint-disable-next-line implicit-arrow-linebreak
-  (
-    <div style={{ width: '1000px', margin: '0 auto' }}>
-      <StudiesSection />
-      {/* <Studies1 /> */}
-      <br /><br /><br /><br /><br />
-    </div>
-  );
+const Studies = () => (
+  <div style={{ width: '1000px', margin: '0 auto' }}>
+    <StudiesSection />
+    {/* <Studies1 /> */}
+    <br /><br /><br /><br /><br />
+  </div>
+);
+
+// const [count, setCount] = useState(0);
+// const [text, setText] = useState('');
+// const [textDone2, setTextDone2] = useState(false);
+// const sentence = '공부한 내용들';
+// useEffect(() => {
+//   const typing = setInterval(() => {
+//     setText(text + sentence[count]);
+//     setCount(count + 1);
+//   }, 150);
+//   if (sentence.length === count) {
+//     clearInterval(typing);
+//     setTimeout(() => {
+//       setTextDone2(true);
+//     }, 1000);
+//   }
+//   return () => {
+//     clearInterval(typing);
+//   };
+// });
+
 export default Studies;
