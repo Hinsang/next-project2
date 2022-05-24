7ba551d3021b6955393d93efff55754152fa2b5e
황인상 <star4381@naver.com>
2022-05-24 21:14:02 +0900

    next-project

--- server/passport/index.js
+++ server/passport/index.js
@@ -7,9 +7,9 @@ module.exports = () => {
     done(null, user.nickname) // 암호화 쿠키와 묶어줄 아이디 정보 저장
   });
 
-  passport.deserializeUser(async (id, done) => {
+  passport.deserializeUser(async (nickname, done) => {
     try {
-      const user = await User.findOne({ where: { id } }) // 쿠키에 묶인 아이디 정보로 사용자 데이터 복원 (이후의 서버요청마다 필요시 복원)
+      const user = await User.findOne({ where: { nickname } }) // 쿠키에 묶인 아이디 정보로 사용자 데이터 복원 (이후의 서버요청마다 필요시 복원)
       done(null, user); // 서버에러, 성공 // 이 부분이 유저정보를 req.user로 변환함
     } catch (error) {
       console.error(error);
