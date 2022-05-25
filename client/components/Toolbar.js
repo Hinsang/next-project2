ed59e7d39b5423d75bead4fda2e639e015b3260d
황인상 <star4381@naver.com>
2022-05-26 08:32:00 +0900

    next-project

--- client/components/Toolbar.js
+++ client/components/Toolbar.js
@@ -20,7 +20,7 @@ function Toolbar() {
   return (
     <div className={styles.inner}>
       <div style={{ height: '10px' }} />
-      <Link href="/">
+      <Link href="/" prefetch={false}>
         {/* <span className={styles.title}>
           NEXT
         </span> */}
@@ -42,7 +42,7 @@ function Toolbar() {
         : (
           <span>
             <span className={styles.toolbar_box}>
-              <Link href="Signup">
+              <Link href="Signup" prefetch={false}>
                 <span className={styles.toolbar}>
                   회원가입
                 </span>
@@ -52,7 +52,7 @@ function Toolbar() {
               |
             </span>
             <span className={styles.toolbar_box}>
-              <Link href="/Login">
+              <Link href="/Login" prefetch={false}>
                 <span className={styles.toolbar}>
                   로그인
                 </span>
@@ -63,19 +63,19 @@ function Toolbar() {
       }
 
       <div className={styles.title_line} />
-      <Link href="/">
+      <Link href="/" prefetch={false}>
         <span className={styles.side_toolbar}>Home</span>
       </Link>
       {/* <Link href="/Board/Post">
         <span className={styles.side_toolbar}>Board</span>
       </Link> */}
-      <Link href="/Studies">
+      <Link href="/Studies" prefetch={false}>
         <span className={styles.side_toolbar}>Studies</span>
       </Link>
-      <Link href="/Mypage">
+      <Link href="/Mypage" prefetch={false}>
         <span className={styles.side_toolbar}>About</span>
       </Link>
-      <Link href="/Introduce">
+      <Link href="/Introduce" prefetch={false}>
         <span className={styles.side_toolbar}>Introduce</span>
       </Link>
       {/* <Link href="/Board">
