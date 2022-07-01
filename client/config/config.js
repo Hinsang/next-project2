4074045400fc91d96f7a69ce3e97deeb00e2fcb3
황인상 <star4381@naver.com>
2022-07-01 09:56:06 +0900

    게시판 추가13

--- client/config/config.js
+++ client/config/config.js
@@ -1,3 +1,3 @@
 // eslint-disable-next-line import/prefer-default-export
-export const backUrl = 'http://api.waxis.co.kr';
+export const backUrl = 'https://api.waxis.co.kr';
 // export const backUrl = 'http://localhost:80';
